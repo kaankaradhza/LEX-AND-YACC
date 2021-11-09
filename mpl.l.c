87dc9ee3afcd384c1b88e9d03c5defb88f615083
kaankaradhza <93927251+kaankaradhza@users.noreply.github.com>
2021-11-09 14:01:23 +0300

    Add files via upload

---
+++ mpl.l.c
@@ -0,0 +1,37 @@
+%{
+    #include<stdio.h>
+    #include "y.tab.h"
+    #include<stdlib.h>
+    #include "mytable.h"
+%}
+letter [a-zA-Z]
+digit [0-9]
+%%
+"+"              return ADDITION;
+"-"              return SUBTRACTION;
+"*"              return MULTIPLICATION;
+"/"              return DIVISION;
+"and"            return AND;
+"or"             return OR;
+"<="             return LESSER_EQUAL;
+">="             return GREATER_EQUAL;
+"=="             return EQUAL;
+"="              return ASSIGNMENT;
+"if"             return IF;
+"else"           return ELSE;
+"while"          return WHILE;
+"break"          return BREAK;
+"int"            return INTEGER;
+"float"          return FLOAT;
+"char"           return CHAR;
+{letter}({letter}|{digit})* return IDENTIFIER;
+{digit}+ return NUM;
+";" return SEMICOLON;
+"," return COMMA;
+"(" return OPEN_PARANTHESIS;
+")" return CLOSE_PARANTHESIS;
+"{" return OPEN_BRACES;
+"}" return CLOSE_BRACES;
+\n
+[\t]+
+%%
---
+++ mpl.y.c
@@ -0,0 +1,58 @@
+%{
+	#include<stdio.h>
+	#include<stdlib.h>
+	#include<string.h>
+	void yyerror(const char *str)
+{
+    fprintf(stderr, "error: %s\n", str);
+}
+int yywrap()
+{
+    return 1;
+}
+main()
+{
+    yyparse();
+}
+
+%}
+
+%token IDENTIFIER NUM
+%token INTEGER FLOAT CHAR
+%token AND OR LESSER_EQUAL GREATER_EQUAL EQUAL ASSIGNMENT
+%token IF ELSE WHILE BREAK
+%token ADDITION SUBTRACTION MULTIPLICATION DIVISION
+%token SEMICOLON COMMA
+%token OPEN_PARANTHESIS CLOSE_PARANTHESIS OPEN_BRACES CLOSE_BRACES
+%start program
+
+%%
+program
+        :(declarations function {printf("Input accepted\n");exit(0);}
+        ;
+declarations
+        :type variables SEMICOLON
+        |
+        ;
+type
+        :INTEGER
+        |FLOAT
+        |CHAR
+        ;
+variables
+        :IDENTIFIER
+        |IDENTIFIER COMMA variables
+function
+        :type IDENTIFIER OPEN_PARANTHESIS arguementlist CLOSE_PARANTHESIS OPEN_BRACES statements CLOSE_BRACES
+        ;
+argumentlist
+        :variables
+        ;
+statements
+        :expressions SEMICOLON
+        ;
+expressions
+        :IDENTIFIER ASSIGNMENT expressions
+        |IDENTIFIER
+        |NUM
+        ;
